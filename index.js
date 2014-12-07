36c28b3ee315e7d5fca615bff98b3bed31107301
deliverymanager <info@deliverymanager.gr>
2014-12-07 16:17:45 +0200

    chnages

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ bugsnag.register("9d5907a30dcfaf8806e542fbf61cf623");
 //This connection will be used in all the controllers and middleware in the app.
 //mongoDbConnection(function(db) {});
 
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses);
+
 //Here I am placing the cron jobs
 //var cron = require("./cronjobs");
 app.use(function(req, res, next) {
