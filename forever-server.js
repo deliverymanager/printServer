20eab66130d38c1c7f9f8e336e208987a9397175
unknown <info@deliverymanager.gr>
2014-12-11 01:31:48 +0200

    chnage

--- forever-server.js
+++ forever-server.js
@@ -8,25 +8,30 @@ var child = new(forever.Monitor)('server.js', {
 });
 
 child.on('exit', function() {});
-//The server process is not dependent on the the git pull
+//The server process is not dependent on the git pull process
 child.start();
 
-
-//The problem with this script is the fact that the script will not run but in the start of the computer when it will not have internet connection!
-//So I need to execute the process when there is internet connection. Probably using a cron or something else
-var sys = require('sys');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { 
-
-	sys.puts(stdout);
+var CronJob = require('cron').CronJob;
+new CronJob('00 00 12 * * *', function(){
 	
-	if (error == null) { // The error might be for example that the user has no internet connection.
-		//Here I restart the server.js child process because the git pull was just called.
-		child.restart();
-    }
-	
-}
+	//The problem with this script is the fact that the script will not run but in the start of the computer when it will not have internet connection!
+	//So I need to execute the process when there is internet connection. Probably using a cron or something else
+	//This process cannot be placed inside the server.js script because it is already a child process.
+	var sys = require('sys');
+	var exec = require('child_process').exec;
+	function puts(error, stdout, stderr) { 
 
-var options = {cwd:"C:\\printServer"};
+		sys.puts(stdout);
+		
+		if (error == null) { // The error might be for example that the user has no internet connection.
+			//Here I restart the server.js child process because the git pull was just called.
+			child.restart();
+		}
+		
+	}
 
-exec("git status && git reset --hard && git pull", options, puts);
+	var options = {cwd:"C:\\printServer"};
+
+	exec("git status && git reset --hard && git pull", options, puts);
+
+}, null, true, "Europe/Athens");
