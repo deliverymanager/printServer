21f0ff432de8e822e3f094024acaef9b1fcb17ca
unknown <info@deliverymanager.gr>
2014-12-10 23:31:17 +0200

    chnage

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/printServer', function(req, res) {
     console.log("/printServer was just called");
     //res.json is used usually when I want to return data from an API
     res.json({
-        message: 'Live v5'
+        message: 'Live  git pull'
     });
 });
 
