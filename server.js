b751eb33c58ca417ec6504bea2caeaef6b56b625
unknown <info@deliverymanager.gr>
2014-12-11 00:24:19 +0200

    changes'

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var bugsnag = require("bugsnag");
 bugsnag.register("9d5907a30dcfaf8806e542fbf61cf623");
-/*
-pull = require('pull').set('http://github.com/deliverymanager/printServer', 'C:\\printServer', function () {
-	pull.syncDirectly();
-})
-*/
 
 var sys = require('sys')
 var exec = require('child_process').exec;
@@ -20,39 +15,6 @@ var options = {cwd:"C:\\printServer"};
 
 exec("git status && git pull", options, puts);
 
-/*
-var forever = require('forever-monitor');
-
-var child = new(forever.Monitor)('index.js', {
-    max: 3,
-    silent: false,
-    args: []
-});
-
-child.on('exit', function() {
-    console.log('index.js has exited after 3 restarts');
-});
-
-child.start();
-*/
-/*
-
-var gitPullCron = require('git-pull-cron');
-- Clone given repo into /dev/my-repo, replacing what's already there
-- Schedule cron to run every weekday (Mon-Fri) at 11:30am
-- When cron task runs, a `git pull origin master` will be performed
-- Once cron task has run the callback will get invoked with latest commit info
- */
-/*
-gitPullCron.init('https://github.com/deliverymanager/printServer', './Applications', '00 00 * * * *', function(err, commit) {
-    if (err) {
-        return console.error(err.stack);
-    }
-
-    console.log('Updated to commit: ' + commit.id);
-});
-*/
-
 //Here I am creating the singleton connection to the MongoDb server.
 //This connection will be used in all the controllers and middleware in the app.
 //mongoDbConnection(function(db) {});
@@ -60,49 +22,11 @@ gitPullCron.init('https://github.com/deliverymanager/printServer', './Applicatio
 var os = require('os');
 console.log(os.platform());
 console.log(os.type());
-if (os.type() == "Windows_NT") {
-
-    //First we need to check if the service is already installed
-
-
-    //If no then we check if the user has administrative priviledges
-    //On windows XP nothing with happen as most of the times the user is admin
-    //On Windows 7 etc, the pop prompt will appear asking for administrative priviledges
-    /*
-    var wincmd = require('node-windows');
-
-    wincmd.isAdminUser(function(isAdmin) {
-        if (isAdmin) {
-            console.log('The user has administrative privileges.');
-            //Then we need to install and start the service!
-            var Service = require('node-windows').Service;
-
-            // Create a new service object
-            var svc = new Service({
-                name: 'Delivery Manager Print Server',
-                description: 'deliverymanager.gr',
-                script: 'C:\\printServer\\index.js'
-            });
-
-            // Listen for the "install" event, which indicates the
-            // process is available as a service.
-            svc.on('install', function() {
-                svc.start();
-            });
-
-            svc.install();
-
-        } else {
-            console.log('NOT AN ADMIN');
-        }
-    });
-    */
-}
 
 console.log(os.hostname());
 console.log(os.arch());
 console.log(os.release());
-console.log(os.cpus());
+
 var interfaces = os.networkInterfaces();
 var addresses = [];
 for (var k in interfaces) {
@@ -135,7 +59,7 @@ app.get('/printServer', function(req, res) {
     console.log("/printServer was just called");
     //res.json is used usually when I want to return data from an API
     res.json({
-        message: 'Live  git pull works or not? HA?'
+        message: 'Live'
     });
 });
 
