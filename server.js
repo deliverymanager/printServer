afe800528892f38ee3208b19e76e9aba4ee609e8
deliverymanager <info@deliverymanager.gr>
2014-12-11 13:17:12 +0200

    chnages' gitk

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.post('/printOrder', function(req, res) {
         success: function(jobID) {
             console.log("sent to printer with ID: " + jobID);
             res.json({
-                message: req
+                message: data.printer
             });
         },
         error: function(err) {
